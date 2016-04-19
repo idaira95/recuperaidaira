4edf632fb4144274d606c11e62ba0ad5bc644499
Idaira Isabel <Idaira Isabel>
2016-04-19 20:32:52 +0200

    paso 1 y 2

---
+++ UT5PruebaRecuperacionIdairaArtiles.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ut5pruebarecuperacionidaira;
+
+public class Ordenador {
+
+
+    private String procesador;
+    private String memoria;
+    private String resolucionPantalla;
+    
+   
+}
